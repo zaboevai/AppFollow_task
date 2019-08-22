9cfe199f742efbc3e4d5790b85a0f0a8827f0f04
zaboevai <zaboevai@yandex.ru>
2019-08-22 09:55:49 +0300

    added 1) test_news_data 2) select from db

--- main.py
+++ main.py
@@ -1,75 +1,52 @@
-import time
-from html.parser import HTMLParser
-import requests
-
-from news_data_base import DataBase, News
-
+from data_base.core import DataBase
+from data_base.tables import News
 
 SOURCE_URL = 'https://news.ycombinator.com'
 
-# NEWS_TEMPLATE = [
-#     {"id": 1,
-#      "title": "Announcing Rust 1.33.0",
-#      "url": "​https://example.com​ ",
-#      "created": "ISO 8601"},
-# ]
+NEWS_TEST_DATA = [
+    {"title": "title_1",
+     "url": "https://example.com",
+     "created": "ISO 8601"},
 
-responce = requests.get(url=SOURCE_URL, )
+    {"title": "title_2",
+     "url": "https://example.com",
+     "created": "ISO 8601"},
 
+    {"title": "title_3",
+     "url": "https://example.com",
+     "created": "ISO 8601"},
 
-# print(responce.text)
-
-
-class NewsParser(HTMLParser):
-
-    def __init__(self, url, news_count=5, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.is_find = False
-        self.fresh_news = []
-        self.news = {}
-        self.count = 1
-
-    def handle_starttag(self, tag, attrs):
-        attrs = dict(attrs)
-
-        if not (tag == 'a' and 'storylink' in attrs.values()):
-            self.is_find = False
-            return
-
-        self.news.clear()
-        self.is_find = True
-        self.news['id'] = len(self.fresh_news) + 1
-        self.news['url'] = attrs['href']
-
-    def handle_endtag(self, tag):
-        if self.is_find:
-            news = self.news.copy()
-            self.fresh_news.append(news)
-            # print(self.news)
-
-    def handle_data(self, data):
-        if self.is_find:
-            self.news['title'] = data
-            self.news['created'] = time.localtime()
-
-    def get_news(self):
-        return self.fresh_news
-
-
-parser = NewsParser(url=SOURCE_URL)
-parser.feed(responce.text)
-
-
+    {"title": "title_4",
+     "url": "https://example.com",
+     "created": "ISO 8601"},
+]
+# create db and table
 NewsDB = DataBase()
 NewsDB.create_db()
 NewsDB.create_tables()
-
 session = NewsDB.get_session()
 
-for news in parser.get_news():
-    session.add(News(**news))
+# insert
+NewsDB.insert(table=News, rows=NEWS_TEST_DATA)
 
-session.commit()
+# select
+news_data = session.query(News).filter()
+for news in news_data:
+    print(news.id, news.title, news.url, news.created)
 
-s = session.query(News.id).filter(News.id == 1)
-print(s.all())
+"""
+decompose task:
+
+NewsParser:
+1) create table (if needed)
+2) get_news
+3) insert news into table
+4) update data
+
+API
+1) parse request
+2) get data from table
+3) sorting (if needed)
+4) send response
+
+"""
