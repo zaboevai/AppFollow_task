e86f4f459d900a58259cf3b72c81ea60a84befd6
zaboevai <zaboevai@yandex.ru>
2019-08-24 16:43:58 +0300

    added request from db with params

--- data_base/core.py
+++ data_base/core.py
@@ -32,17 +32,39 @@ class DataBase:
         total_rows = self.db.session.query(News).filter().count()
         return total_rows
 
-    def get_json_rows_from_db(self):
+    def get_json_rows_from_db(self, news_limit=None, offset=None, order_by=None, order_by_desc=None):
         if not self.db.session:
             raise BaseException('Вставка невозможна, не удалось определить сессию')
 
-        news_data = self.db.session.query(News).filter()
+        news_data = self.db.session.query(News).order_by(News.created.desc()).offset(offset).limit(news_limit)
+
+        if order_by_desc:
+            if order_by_desc == 'title':
+                news_data = news_data.from_self().order_by(News.title.desc()).all()
+            elif order_by_desc == 'created':
+                news_data = news_data.from_self().order_by(News.created.desc()).all()
+            elif order_by_desc == 'url':
+                news_data = news_data.from_self().order_by(News.url.desc()).all()
+            elif order_by_desc == 'id':
+                news_data = news_data.from_self().order_by(News.id.desc()).all()
+        else:
+            if order_by == 'title':
+                news_data = news_data.from_self().order_by(News.title).all()
+            elif order_by == 'created':
+                news_data = news_data.from_self().order_by(News.created).all()
+            elif order_by == 'url':
+                news_data = news_data.from_self().order_by(News.url).all()
+            elif order_by == 'id':
+                news_data = news_data.from_self().order_by(News.id).all()
+
         rows = []
+        news_id = 0
         for news in news_data:
-            rows.append({'id': news.id,
+            news_id += 1
+            rows.append({'id': news_id,
                          'title': news.title,
                          'url': news.url,
-                         'created': news.created.isoformat()
+                         'created': news.created.isoformat(" ", "seconds")
                          })
 
         json_rows = json.dumps(rows, indent=4)
